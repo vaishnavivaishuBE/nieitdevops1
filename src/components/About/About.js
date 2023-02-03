3dd8b5d5d8b80f58bf3cc87070de48b43c4acdd5
vaishnavivaishuBE <vaishnavivaishu9035@gmail.com>
2023-02-03 12:02:53 +0530

    test1
    react project

---
+++ src/components/About/About.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <div className="banner-image-about">
+        <div className="about-nike">
+          <h2 className="about-nike-text">OUR MISSION</h2>
+          <p>
+            "BRING INSPIRATION AND INNOVATION TO EVERY ATHLETE* IN THE WORLD"
+          </p>
+        </div>
+      </div>
+      <div className="about-nike-part">
+        <h2 className="about-nike-heading">Abouuuuuuuuut Nike</h2>
+        <p className='about-nike-para'>
+          Our misjjjjjjjsion is what drives us to do everything possible to expand
+          human potential. We do that by creating groundbreaking sport
+          innovations, by making our phhhhhhroducts more sustainably, by building a
+          creative and diverse global team and by making a positive impact in
+          communities where we live and work. Based in Beaverton, Oregon, NIKE,
+          Inc. includes the Nike, Cojsrbvosrubosnverse, and Jordan brands.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default About;
